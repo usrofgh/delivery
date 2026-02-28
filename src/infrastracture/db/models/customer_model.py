14c4674fb6401ca0f983cced272e3d745c466fc9
Developer <notemail@notemail.com>
2026-02-28 02:50:05 +0200

    DEL-27: Changed customer ORM model for registration

--- src/infrastracture/db/models/customer_model.py
+++ src/infrastracture/db/models/customer_model.py
@@ -4,6 +4,7 @@ from uuid import UUID
 from sqlalchemy import DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 
+from domain.enums.account_status import AccountStatus
 from infrastracture.db.models.base_model import BaseModel
 
 
@@ -12,7 +13,8 @@ class CustomerModel(BaseModel):
 
     id: Mapped[UUID] = mapped_column(primary_key=True)
     name: Mapped[str]
-    email: Mapped[str]
-    phone: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
     hashed_password: Mapped[str]
+    status: Mapped[AccountStatus]
+    activated_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
